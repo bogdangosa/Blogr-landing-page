de6b1b8eca159a6ee998dcbde57e6940772f4672
bogdangosa <57357871+bogdangosa@users.noreply.github.com>
2021-11-07 22:36:34 +0200

    update mobile navigation

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 let Main_links = document.getElementsByClassName("main-link");
 let Secondary_lists = document.getElementsByClassName("secondary-list");
+let NavLinks = document.getElementById("main-list-links");
 
 Array.from(Main_links).forEach((link,index) => {
 
     link.addEventListener('click',()=>{
         Secondary_lists[index].classList.toggle("visible");
-        console.log(Secondary_lists[index].classList);
+        for(let i=0;i<Main_links.length;i++){
+            if(i!=index)
+                Secondary_lists[i].classList.remove("visible");
+        }
     });
+});
+
+let Hamburger = document.getElementById("hamburger");
+Hamburger.addEventListener('click',()=>{
+    let TopHeader = document.getElementById("top-header");
+    TopHeader.classList.toggle("top-header-nav-visible");
+    NavLinks.classList.toggle("visible");
 });
